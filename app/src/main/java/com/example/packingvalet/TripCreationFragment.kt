07c9e7916d07dbde0b503d7512feae337c68c515
nietup <jakub.nietupski@gmail.com>
2021-12-28 14:33:15 +0100

    Setup transitions

--- app/src/main/java/com/example/packingvalet/TripCreationFragment.kt
+++ app/src/main/java/com/example/packingvalet/TripCreationFragment.kt
@@ -5,53 +5,30 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.findNavController
+import com.example.packingvalet.databinding.FragmentTripCreationBinding
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-
-/**
- * A simple [Fragment] subclass.
- * Use the [TripCreationFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class TripCreationFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
-    }
+    private var binding: FragmentTripCreationBinding? = null
 
-    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
+    override fun onCreateView(inflater: LayoutInflater,
+                              container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_trip_creation, container, false)
+        super.onCreateView(inflater, container, savedInstanceState)
+        binding = FragmentTripCreationBinding.inflate(inflater, container, false)
+
+        return binding?.root
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment TripCreationFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            TripCreationFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding?.button?.setOnClickListener {
+            binding?.root?.findNavController()?.navigate(
+                TripCreationFragmentDirections
+                    .actionTripCreationFragmentToPackingActionFragment()
+            )
+        }
     }
 }
