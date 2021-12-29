e4a8fd80977ecd1c5ebde567a3d297cca2bee9e4
nietup <jakub.nietupski@gmail.com>
2021-12-29 12:10:57 +0100

    packing action ui v1

--- app/src/main/java/com/example/packingvalet/PackingActionFragment.kt
+++ app/src/main/java/com/example/packingvalet/PackingActionFragment.kt
@@ -5,53 +5,56 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ArrayAdapter
+import androidx.navigation.findNavController
+import com.example.packingvalet.databinding.FragmentPackingActionBinding
+import com.example.packingvalet.databinding.FragmentTripCreationBinding
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-
-/**
- * A simple [Fragment] subclass.
- * Use the [PackingActionFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class PackingActionFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
-    }
+    private var binding: FragmentPackingActionBinding? = null
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_packing_action, container, false)
+        super.onCreateView(inflater, container, savedInstanceState)
+        binding = FragmentPackingActionBinding.inflate(inflater, container, false)
+
+        return binding?.root
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment PackingActionFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            PackingActionFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val items = getItems()
+        val adapter = ArrayAdapter(requireContext(),
+                                   R.layout.support_simple_spinner_dropdown_item,
+                                   items)
+
+        binding?.selectedItemList?.adapter = adapter
     }
+
+    private fun getItems() = listOf("Pants",
+                                    "Toothbrush",
+                                    "Charger",
+                                    "Vifon",
+                                    "Pants",
+                                    "Toothbrush",
+                                    "Charger",
+                                    "Vifon",
+                                    "Pants",
+                                    "Toothbrush",
+                                    "Charger",
+                                    "Vifon",
+                                    "Pants",
+                                    "Toothbrush",
+                                    "Charger",
+                                    "Vifon",
+                                    "Pants",
+                                    "Toothbrush",
+                                    "Charger",
+                                    "Vifon",
+                                    "Pants",
+                                    "Toothbrush",
+                                    "Charger",
+                                    "Vifon")
 }
