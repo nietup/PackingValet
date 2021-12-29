727fac39a9821f09eed2a5d84dd4e3d3704bd4fb
nietup <jakub.nietupski@gmail.com>
2021-12-29 11:47:20 +0100

    trip creation design v1

--- app/src/main/java/com/example/packingvalet/TripCreationFragment.kt
+++ app/src/main/java/com/example/packingvalet/TripCreationFragment.kt
@@ -5,6 +5,7 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ArrayAdapter
 import androidx.navigation.findNavController
 import com.example.packingvalet.databinding.FragmentTripCreationBinding
 
@@ -30,5 +31,37 @@ class TripCreationFragment : Fragment() {
                     .actionTripCreationFragmentToPackingActionFragment()
             )
         }
+
+        val items = getItems()
+        val adapter = ArrayAdapter(requireContext(),
+                                   R.layout.support_simple_spinner_dropdown_item,
+                                   items)
+
+        binding?.itemList?.adapter = adapter
     }
+
+    private fun getItems() = listOf("Pants",
+                                    "Toothbrush",
+                                    "Charger",
+                                    "Vifon",
+                                    "Pants",
+                                    "Toothbrush",
+                                    "Charger",
+                                    "Vifon",
+                                    "Pants",
+                                    "Toothbrush",
+                                    "Charger",
+                                    "Vifon",
+                                    "Pants",
+                                    "Toothbrush",
+                                    "Charger",
+                                    "Vifon",
+                                    "Pants",
+                                    "Toothbrush",
+                                    "Charger",
+                                    "Vifon",
+                                    "Pants",
+                                    "Toothbrush",
+                                    "Charger",
+                                    "Vifon")
 }
