11917cfe0ec391ae623403201acafabeea474b98
nietup <jakub.nietupski@gmail.com>
2021-12-29 16:30:08 +0100

    add new item logic

--- app/src/main/java/com/example/packingvalet/TripCreationFragment.kt
+++ app/src/main/java/com/example/packingvalet/TripCreationFragment.kt
@@ -1,16 +1,20 @@
 package com.example.packingvalet
 
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ArrayAdapter
+import android.widget.Toast
 import androidx.navigation.findNavController
 import com.example.packingvalet.databinding.FragmentTripCreationBinding
 
 class TripCreationFragment : Fragment() {
 
+    private val items = mutableListOf("Pants", "Toothbrush", "Charger", "Vifon")
     private var binding: FragmentTripCreationBinding? = null
 
     override fun onCreateView(inflater: LayoutInflater,
@@ -32,36 +36,27 @@ class TripCreationFragment : Fragment() {
             )
         }
 
-        val items = getItems()
         val adapter = ArrayAdapter(requireContext(),
                                    R.layout.support_simple_spinner_dropdown_item,
                                    items)
 
         binding?.storedItemList?.adapter = adapter
-    }
 
-    private fun getItems() = listOf("Pants",
-                                    "Toothbrush",
-                                    "Charger",
-                                    "Vifon",
-                                    "Pants",
-                                    "Toothbrush",
-                                    "Charger",
-                                    "Vifon",
-                                    "Pants",
-                                    "Toothbrush",
-                                    "Charger",
-                                    "Vifon",
-                                    "Pants",
-                                    "Toothbrush",
-                                    "Charger",
-                                    "Vifon",
-                                    "Pants",
-                                    "Toothbrush",
-                                    "Charger",
-                                    "Vifon",
-                                    "Pants",
-                                    "Toothbrush",
-                                    "Charger",
-                                    "Vifon")
+        binding?.newItemEditText?.addTextChangedListener(object : TextWatcher {
+            override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+            }
+
+            override fun onTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+            }
+
+            override fun afterTextChanged(p0: Editable?) {
+                p0.toString().let {
+                    if (it.contains('\n')) {
+                        items.add(it.trim())
+                        p0?.clear()
+                    }
+                }
+            }
+        })
+    }
 }
